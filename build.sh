747b73d9ecb3b574f8dd9b57c88211a81c7d690d
Bill Chatfield <bill_chatfield@yahoo.com>
2021-04-20 14:09:49 -0400

    Parameterized build and added clean

--- build.sh
+++ build.sh
@@ -1,16 +1,26 @@
 #!/bin/sh
 # No need to install Ant, Maven, or Gradle. Ever tried running Gradle on a PowerPC G4? Hahahahahaaa... not.
 # This is faster and simpler than any build system. Can you say "over-engineered"?
+
+BUILD_DIR=bin
+TARGET_JAR=magic-conch-shell.jar
+
+if [ "$1" = "clean" ]
+then
+  rm -rf "$BUILD_DIR" "$TARGET_JAR"
+  exit
+fi
+
 CLASSPATH=lib/bsf-api-3.1.jar
 # shellcheck disable=SC2006
 SOURCES=`find . -name '*.java' -print`
-rm -rf bin || exit
-mkdir bin || exit
+rm -rf "$BUILD_DIR" || exit
+mkdir "$BUILD_DIR" || exit
 # shellcheck disable=SC2086
-javac -classpath $CLASSPATH $SOURCES -d bin || exit
-cd bin || exit
+javac -classpath $CLASSPATH $SOURCES -d "$BUILD_DIR" || exit
+cd "$BUILD_DIR" || exit
 # This won't work until after javac generates some classes.
 # shellcheck disable=SC2006
 CLASSES=`find . -name '*.class' -print`
 # shellcheck disable=SC2086
-jar -cvf ../magic-conch-shell.jar $CLASSES || exit
+jar -cvf ../"$TARGET_JAR" $CLASSES || exit
